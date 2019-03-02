4779d77c38da3b78d78fa2013da28556aab28952
Kevin Wang <rocwang0316@gmail.com>
2019-03-01 17:55:10 -0800

    Set up Redux.

---
+++ app/reducers/Index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+// import { routerReducer } from 'react-router-redux';
+
+// import basicInformation from './BasicInformationReducers';
+
+export default combineReducers({
+});
