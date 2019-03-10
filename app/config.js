b8141f872720902dc05c675e7c35b9c714cd9926
Kevin Wang <rocwang0316@gmail.com>
2019-03-09 17:14:39 -0800

    Added the Cognito configuration.

--- app/config.js
+++ app/config.js
@@ -1,3 +1,9 @@
 export const BASE_URL = ''; // Production server
 /* URLS */
 export const HOME_PAGE_URL = '/';
+
+export const cognitoConfig = {
+  userPoolId: 'us-west-2_dpFcYh6zK',
+  region: 'us-west-2',
+  userPoolWebClientId: '60pcdu4n96ko52j31gl493vnf8',
+};
