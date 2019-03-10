b2b82bdaeed8479b7369a20bb89e65ec3596aecf
Kevin Wang <rocwang0316@gmail.com>
2019-03-09 18:01:53 -0800

    Added the amplifyAuthSignOption

--- app/config.js
+++ app/config.js
@@ -5,5 +5,19 @@ export const HOME_PAGE_URL = '/';
 export const cognitoConfig = {
   userPoolId: 'us-west-2_dpFcYh6zK',
   region: 'us-west-2',
-  userPoolWebClientId: '60pcdu4n96ko52j31gl493vnf8',
+  userPoolWebClientId: 'jr91pfromc0iu9s0ee6cb4t8o',
+};
+
+export const amplifyAuthSignOption = {
+  signUpConfig: {
+    hiddenDefaults: ['phone_number', 'email', 'username'],
+    signUpFields: [
+      {
+        label: 'Nickname', key: 'nickname', required: true, type: 'string', displayOrder: 1,
+      },
+      {
+        label: 'Email', key: 'email', required: true, type: 'string', displayOrder: 2,
+      },
+    ],
+  },
 };
