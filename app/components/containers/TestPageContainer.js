32122c09b8ca986cfeb13c2b020360b22228d4fa
Kevin Wang <rocwang0316@gmail.com>
2019-03-11 16:52:19 -0700

    Gave the test page container a auth check functionality. It should be abstracted somehow.

--- app/components/containers/TestPageContainer.js
+++ app/components/containers/TestPageContainer.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withAuthenticator } from 'aws-amplify-react';
 import { Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Auth } from 'aws-amplify';
 
 import { amplifyAuthSignOption } from '../../config';
+import { loginSuccess as loginSuccessAction } from '../../actions/UserActions';
 
-const TestPageContainer = () => (
-  <Typography color="textPrimary" variant="h6">This is the Test Page</Typography>
+const TestPageContainer = ({ user, loginSuccess }) => {
+  useEffect(() => {
+    if (!user) {
+      Auth.currentAuthenticatedUser()
+        .then(cognitoUser => loginSuccess(cognitoUser))
+        .catch(err => console.log(err));
+    }
+  });
+  return <Typography color="textPrimary" variant="h6">This is the Test Page</Typography>;
+};
+
+TestPageContainer.propTypes = {
+  user: PropTypes.object,
+  loginSuccess: PropTypes.func.isRequired,
+};
+TestPageContainer.defaultProps = { user: null };
+
+/* istanbul ignore next */
+const mapStateToProps = state => ({ user: state.user });
+/* istanbul ignore next */
+const mapDispatchToProps = dispatch => ({
+  loginSuccess: user => dispatch(loginSuccessAction(user)),
+});
+
+export default withAuthenticator(
+  connect(mapStateToProps, mapDispatchToProps)(TestPageContainer),
+  amplifyAuthSignOption,
 );
-export default withAuthenticator(TestPageContainer, amplifyAuthSignOption);
