0a29663f4319581190b074a8f8a1b555f96be16d
Kevin Wang <rocwang0316@gmail.com>
2019-03-13 11:01:34 -0700

    Added new words to the dictionary'

--- app/Dict.js
+++ app/Dict.js
@@ -1,15 +1,19 @@
 export default {
   en: {
     login: 'login',
+    logout: 'logout',
     test: 'test',
     other: 'other',
+    appName: 'SSCRG CLUB',
     homePageContent: 'This is the Home Page',
     testPageContent: 'This is the Test Page',
   },
   zh: {
     login: '登录',
+    logout: '登出',
     test: '测试',
     other: '其他',
+    appName: 'SSCRG 俱乐部',
     homePageContent: '这是首页',
     testPageContent: '这是测试页',
   },
