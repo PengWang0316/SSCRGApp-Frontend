9a34ba17914620bfe8fab8a3975a714d6d0eb53a
Kevin Wang <rocwang0316@gmail.com>
2019-03-12 20:27:40 -0700

    Added I18n language setup

--- app/index.js
+++ app/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
 import { PersistGate } from 'redux-persist/integration/react';
+import { I18n } from 'aws-amplify';
 
 import App from './components/App';
 import configureStore from './store/ConfigureStore';
 import './styles/index.global.css';
 import registerServiceWorker from './registerServiceWorker';
+import { DEFAULT_LANGUAGE } from './config';
+import dict from './Dict';
 
 const { store, persistor } = configureStore();
+
+// Fallback to english if cannot detect the language
+const language = (navigator.language || navigator.userLanguage || DEFAULT_LANGUAGE).slice(0, 2);
+// Fallback to english if dict does not have the user's language
+I18n.setLanguage(dict[language] ? language : DEFAULT_LANGUAGE);
+I18n.putVocabularies(dict);
 
 // ReactDOM.render(
 //   <Provider store={store}>
