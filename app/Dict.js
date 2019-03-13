6df4e3bfa4eca263e098c38c59aad177c4e2e961
Kevin Wang <rocwang0316@gmail.com>
2019-03-12 20:28:02 -0700

    Added the dictionary.

---
+++ app/Dict.js
@@ -0,0 +1,16 @@
+export default {
+  en: {
+    login: 'login',
+    test: 'test',
+    other: 'other',
+    homePageContent: 'This is the Home Page',
+    testPageContent: 'This is the Test Page',
+  },
+  zh: {
+    login: '登录',
+    test: '测试',
+    other: '其他',
+    homePageContent: '这是首页',
+    testPageContent: '这是测试页',
+  },
+};
