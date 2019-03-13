8d5ce43a91518ac67d6881a2b657c63cebab8564
Kevin Wang <rocwang0316@gmail.com>
2019-03-12 19:36:47 -0700

    git status

--- app/reducers/User.js
+++ app/reducers/User.js
@@ -3,7 +3,11 @@ import { USER_LOGIN_SUCCESS, USER_LOGOUT_SUCCESS } from '../actions/ActionTypes'
 const user = (state = null, { type, cognitoUser }) => {
   switch (type) {
     case USER_LOGIN_SUCCESS:
-      return { nickname: cognitoUser.attributes.nickname };
+      return {
+        nickname: cognitoUser.attributes
+          ? cognitoUser.attributes.nickname
+          : cognitoUser.name,
+      };
     case USER_LOGOUT_SUCCESS:
       return null;
     default:
