833c888d9f25581f1a2fae171936eca776896d92
Kevin Wang <rocwang0316@gmail.com>
2019-03-18 22:27:12 -0700

    Moved the Auth function to the UserAction

--- app/actions/UserActions.js
+++ app/actions/UserActions.js
@@ -1,6 +1,8 @@
+import { Auth } from 'aws-amplify';
+
 import { USER_LOGIN_SUCCESS, USER_LOGOUT_SUCCESS } from './ActionTypes';
 
-export const loginSuccess = cognitoUser => ({
+const loginSuccess = cognitoUser => ({
   type: USER_LOGIN_SUCCESS,
   cognitoUser,
 });
@@ -8,3 +10,7 @@ export const loginSuccess = cognitoUser => ({
 export const logoutSuccess = () => ({
   type: USER_LOGOUT_SUCCESS,
 });
+
+export const currentAuthenticatedUser = () => Auth.currentAuthenticatedUser()
+  .then(user => loginSuccess(user))
+  .catch(err => console.log(err));
