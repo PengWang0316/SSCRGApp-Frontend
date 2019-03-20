3f2bb8bc4c34b3d8bf58e15bd73f7d1670cc1fa4
Kevin Wang <rocwang0316@gmail.com>
2019-03-19 17:20:14 -0700

    Removed the Amplify from components.

--- app/components/containers/HomePageContainer.js
+++ app/components/containers/HomePageContainer.js
@@ -1,19 +1,14 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Typography } from '@material-ui/core';
-import { Auth } from 'aws-amplify';
 import I18n from '@kevinwang0316/i18n';
 import PropTypes from 'prop-types';
 
-import { loginSuccess as loginSuccessAction } from '../../actions/UserActions';
+import { currentAuthenticatedUser as currentAuthenticatedUserAction } from '../../actions/UserActions';
 
-const HomePage = ({ user, loginSuccess }) => {
+const HomePage = ({ user, currentAuthenticatedUser }) => {
   useEffect(() => {
-    if (!user) {
-      Auth.currentAuthenticatedUser()
-        .then(cognitoUser => loginSuccess(cognitoUser))
-        .catch(err => console.log(err));
-    }
+    if (!user) currentAuthenticatedUser();
   });
 
   return <Typography color="textPrimary" variant="h6">{I18n.get('homePageContent')}</Typography>;
@@ -21,7 +16,7 @@ const HomePage = ({ user, loginSuccess }) => {
 
 HomePage.propTypes = {
   user: PropTypes.object,
-  loginSuccess: PropTypes.func.isRequired,
+  currentAuthenticatedUser: PropTypes.func.isRequired,
 };
 HomePage.defaultProps = { user: null };
 
@@ -29,6 +24,6 @@ HomePage.defaultProps = { user: null };
 const mapStateToProps = state => ({ user: state.user });
 /* istanbul ignore next */
 const mapDispatchToProps = dispatch => ({
-  loginSuccess: user => dispatch(loginSuccessAction(user)),
+  currentAuthenticatedUser: user => dispatch(currentAuthenticatedUserAction(user)),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
--- app/components/containers/TestPageContainer.js
+++ app/components/containers/TestPageContainer.js
@@ -3,25 +3,21 @@ import { withAuthenticator } from 'aws-amplify-react';
 import { Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Auth } from 'aws-amplify';
 import I18n from '@kevinwang0316/i18n';
 
 import { amplifyAuthSignOption } from '../../config';
-import { loginSuccess as loginSuccessAction } from '../../actions/UserActions';
+import { currentAuthenticatedUser as currentAuthenticatedUserAction } from '../../actions/UserActions';
 
-const TestPageContainer = ({ user, loginSuccess }) => {
+const TestPageContainer = ({ user, currentAuthenticatedUser }) => {
   useEffect(() => {
-    if (!user) {
-      Auth.currentAuthenticatedUser()
-        .then(cognitoUser => loginSuccess(cognitoUser)).catch(err => console.log(err));
-    }
+    if (!user) currentAuthenticatedUser();
   });
   return <Typography color="textPrimary" variant="h6">{I18n.get('testPageContent')}</Typography>;
 };
 
 TestPageContainer.propTypes = {
   user: PropTypes.object,
-  loginSuccess: PropTypes.func.isRequired,
+  currentAuthenticatedUser: PropTypes.func.isRequired,
 };
 TestPageContainer.defaultProps = { user: null };
 
@@ -29,7 +25,7 @@ TestPageContainer.defaultProps = { user: null };
 const mapStateToProps = state => ({ user: state.user });
 /* istanbul ignore next */
 const mapDispatchToProps = dispatch => ({
-  loginSuccess: user => dispatch(loginSuccessAction(user)),
+  currentAuthenticatedUser: user => dispatch(currentAuthenticatedUserAction(user)),
 });
 
 export default withAuthenticator(
