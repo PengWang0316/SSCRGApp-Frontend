5dde601bdebd8cd93e3a77b67d597597eb0d348a
Kevin Wang <rocwang0316@gmail.com>
2019-03-21 22:19:31 -0700

    Added the comment

--- app/actions/UserActions.js
+++ app/actions/UserActions.js
@@ -11,10 +11,18 @@ const logoutSuccess = () => ({
   type: USER_LOGOUT_SUCCESS,
 });
 
+/**
+ * Get the current login user informaiton and dispatch it to the Redux state
+ * @return {function} Return a function with dispatch.
+ */
 export const currentAuthenticatedUser = () => dispatch => Auth.currentAuthenticatedUser()
   .then(user => dispatch(loginSuccess(user)))
   .catch(err => console.log(err));
 
+/**
+ * Perform a logout action via aws-amplify Auth function and clear the Redux state
+ * @return {function} Return a function with dispatch.
+ */
 export const logout = () => dispatch => Auth.signOut()
   .then(() => dispatch(logoutSuccess()))
   .catch(err => console.log(err));
